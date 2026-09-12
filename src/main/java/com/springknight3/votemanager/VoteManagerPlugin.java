cfa65b059c80871ead32b3579ca22ab098811040
Spring_Knight3 <elikindle69@gmail.com>
2026-09-12 20:24:21 +0000

    add "/redeem" command + build

--- src/main/java/com/springknight3/votemanager/VoteManagerPlugin.java
+++ src/main/java/com/springknight3/votemanager/VoteManagerPlugin.java
@@ -3,4 +3,8 @@ package com.springknight3.votemanager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VoteManagerPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getCommand("redeem").setExecutor((sender, command, label, args) -> true);
+	}
 }
